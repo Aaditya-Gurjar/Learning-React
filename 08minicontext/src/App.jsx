1eb2f429a4436a5196c9e250cbf818b745c5255e
aaditya <aaditya2002.com@gmail.com>
2024-03-20 13:39:16 +0530

    learnt about context api with project

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './Component/Login'
+import Profile from './Component/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>React is Important!</h1>
+      <Login/>
+      <Profile/>
+
+    </UserContextProvider>
+  )
+}
+
+export default App
