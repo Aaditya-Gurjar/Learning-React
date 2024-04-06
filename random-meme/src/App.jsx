36d68595417b169707fac9b90c9a50ac64c4d1b9
aaditya <aaditya2002.com@gmail.com>
2024-04-06 18:44:49 +0530

    random gif genreator done

--- random-meme/src/App.jsx
+++ random-meme/src/App.jsx
@@ -1,6 +1,15 @@
-
+import React from "react";
+import Random from "./components/Random";
+import Tag from "./components/Tag";
 
 export default function App() {
-  return (<div>Random Gif</div>
+  return (
+  <div className="w-full h-screen flex flex-col background relative overflow-x-hidden">
+    <h1 className="bg-white  text-center font-bold py-2 my-2 mt-5  mx-3 rounded-[10px]  text-black">RANDOM GIF !</h1>
+    <div className="flex flex-col w-full items-center gap-y-10 mt-[30px] ">
+      <Random/>
+      <Tag />
+    </div>
+  </div>
   );
 }
---
+++ random-meme/src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../index.css"
+
+function Spinner() {
+  return (
+    <div>
+        <div className='loader'></div>
+    </div>
+  )
+}
+
+export default Spinner
